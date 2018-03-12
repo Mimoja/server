72e7e1f98bcebd04e4a0adc938795a37612aea06
Mimoja <git@mimoja.de>
2018-03-12 02:26:59 +0100

    cleanup

--- dockers/ovpn/init.sh
+++ dockers/ovpn/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose run --rm openvpn ovpn_genconfig -u udp://vpn.mimoja.de
+docker-compose run --rm openvpn ovpn_genconfig -c -u udp://mimoja.de
 docker-compose run --rm openvpn ovpn_initpki
 
